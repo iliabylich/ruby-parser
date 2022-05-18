220d07eb65ec3a6022b738ee337279cde75d0f1a
Ilya Bylich <ibylich@gmail.com>
2022-05-18 20:05:50 +0200

    fix several warnings

--- src/lexer/ident.rs
+++ src/lexer/ident.rs
@@ -1,6 +1,6 @@
 use crate::lexer::buffer::Buffer;
 use crate::token::Token;
 
-pub(crate) fn parse_ident<'a>(buffer: &mut Buffer<'a>) -> Token<'a> {
+pub(crate) fn parse_ident<'a>(_buffer: &mut Buffer<'a>) -> Token<'a> {
     todo!("parse_ident")
 }
--- src/lexer/percent.rs
+++ src/lexer/percent.rs
@@ -1,6 +1,6 @@
 use crate::lexer::buffer::Buffer;
 use crate::token::Token;
 
-pub(crate) fn parse_percent<'a>(buffer: &mut Buffer<'a>) -> Token<'a> {
+pub(crate) fn parse_percent<'a>(_buffer: &mut Buffer<'a>) -> Token<'a> {
     todo!()
 }
