2b79c1196ff1435bb15f4ed0ce12fda703400a6a
Ilya Bylich <ibylich@gmail.com>
2022-06-04 03:42:47 +0200

    use ControlFlow for all string literal handlers

--- src/lexer/strings/handlers/handle_eof.rs
+++ src/lexer/strings/handlers/handle_eof.rs
@@ -2,7 +2,7 @@ use std::ops::ControlFlow;
 
 use crate::lexer::{
     buffer::Buffer,
-    strings::{action::StringExtendAction, handlers::string_content_to_emit},
+    strings::{action::StringExtendAction, handlers::handle_processed_string_content},
 };
 
 pub(crate) fn handle_eof<'a>(
@@ -10,11 +10,9 @@ pub(crate) fn handle_eof<'a>(
     start: usize,
 ) -> ControlFlow<StringExtendAction> {
     if buffer.is_eof() {
-        if let Some(token) = string_content_to_emit(start, buffer.pos()) {
-            ControlFlow::Break(StringExtendAction::EmitToken { token })
-        } else {
-            ControlFlow::Break(StringExtendAction::EmitEOF)
-        }
+        handle_processed_string_content(start, buffer.pos())?;
+
+        ControlFlow::Break(StringExtendAction::EmitEOF)
     } else {
         ControlFlow::Continue(())
     }
--- src/lexer/strings/handlers/mod.rs
+++ src/lexer/strings/handlers/mod.rs
@@ -12,13 +12,3 @@ pub(crate) use handle_string_end::handle_string_end;
 
 mod handle_processed_string_content;
 pub(crate) use handle_processed_string_content::handle_processed_string_content;
-
-use crate::token::{token, Token};
-
-fn string_content_to_emit(start: usize, end: usize) -> Option<Token> {
-    if start == end {
-        None
-    } else {
-        Some(token!(tSTRING_CONTENT, start, end))
-    }
-}
