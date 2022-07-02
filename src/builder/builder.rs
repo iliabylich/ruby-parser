c0c32f313347099fffec7c5115e3b534779e6267
Ilya Bylich <ibylich@gmail.com>
2022-07-02 19:45:32 +0200

    implement `opt_else`

--- src/builder/builder.rs
+++ src/builder/builder.rs
@@ -347,7 +347,7 @@ impl<'a, C: Constructor> Builder<C> {
     pub(crate) fn begin_body(
         compound_stmt: Box<Node<'a>>,
         rescue_bodies: Vec<Node<'a>>,
-        opt_else: Option<(Token<'a>, Box<Node<'a>>)>,
+        opt_else: Option<(Token<'a>, Option<Box<Node<'a>>>)>,
         opt_ensure: Option<(Token<'a>, Option<Box<Node<'a>>>)>,
     ) -> Box<Node<'a>> {
         todo!("builder.begin_body")
--- src/parser/opt_else.rs
+++ src/parser/opt_else.rs
@@ -1,10 +1,24 @@
-use crate::{builder::Constructor, parser::Parser, token::Token, Node};
+use crate::{
+    builder::Constructor,
+    parser::Parser,
+    token::{Token, TokenValue},
+    Node,
+};
 
 impl<'a, C> Parser<'a, C>
 where
     C: Constructor,
 {
-    pub(crate) fn try_opt_else(&mut self) -> Option<(Token<'a>, Box<Node<'a>>)> {
-        todo!()
+    pub(crate) fn try_opt_else(&mut self) -> Option<(Token<'a>, Option<Box<Node<'a>>>)> {
+        let else_t = self.try_token(TokenValue::kELSE)?;
+        let compstmt = self.try_compstmt();
+        Some((else_t, compstmt))
     }
+}
+
+#[test]
+fn test_opt_else() {
+    use crate::parser::RustParser;
+    let mut parser = RustParser::new(b"else; 42; end");
+    assert_eq!(parser.try_opt_else(), None)
 }
