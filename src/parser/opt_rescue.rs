6ac5d800e44280c879be2a74962a9ad861faa94c
Ilya Bylich <ibylich@gmail.com>
2022-07-06 21:01:33 +0200

    turn private opt_rescue fns into free-standing fns

--- src/parser/opt_rescue.rs
+++ src/parser/opt_rescue.rs
@@ -11,37 +11,13 @@ where
 {
     pub(crate) fn parse_opt_rescue(&mut self) -> Vec<Node<'a>> {
         let mut nodes = vec![];
-        while let Some(node) = self.try_opt_rescue1() {
+        while let Some(node) = try_opt_rescue1(self) {
             nodes.push(*node)
         }
         nodes
     }
 
-    fn try_opt_rescue1(&mut self) -> Option<Box<Node<'a>>> {
-        let rescue_t = self.try_token(TokenValue::kRESCUE)?;
-        let exc_list = self.try_exc_list();
-        let exc_var = self.try_exc_var();
-        let then = self.try_then();
-        let compstmt = self.try_compstmt();
-        Some(Builder::<C>::rescue_body(
-            rescue_t, exc_list, exc_var, then, compstmt,
-        ))
-    }
-
-    fn try_exc_list(&mut self) -> Option<Vec<Node<'a>>> {
-        None.or_else(|| self.try_arg_value().map(|arg_value| vec![*arg_value]))
-            .or_else(|| self.try_mrhs())
-    }
-    fn try_exc_var(&mut self) -> Option<(Token<'a>, Box<Node<'a>>)> {
-        let assoc_t = self.try_token(TokenValue::tASSOC)?;
-        if let Some(lhs) = self.try_lhs() {
-            Some((assoc_t, lhs))
-        } else {
-            panic!("error: expected LHS after =>")
-        }
-    }
-
-    fn try_then(&mut self) -> Option<Token<'a>> {
+    pub(crate) fn try_then(&mut self) -> Option<Token<'a>> {
         None.or_else(|| self.try_term())
             .or_else(|| self.try_token(TokenValue::kTHEN))
             .or_else(|| {
@@ -97,13 +73,40 @@ where
     }
 }
 
+fn try_opt_rescue1<'a, C: Constructor>(parser: &mut Parser<'a, C>) -> Option<Box<Node<'a>>> {
+    let rescue_t = parser.try_token(TokenValue::kRESCUE)?;
+    let exc_list = try_exc_list(parser);
+    let exc_var = try_exc_var(parser);
+    let then = parser.try_then();
+    let compstmt = parser.try_compstmt();
+    Some(Builder::<C>::rescue_body(
+        rescue_t, exc_list, exc_var, then, compstmt,
+    ))
+}
+
+fn try_exc_list<'a, C: Constructor>(parser: &mut Parser<'a, C>) -> Option<Vec<Node<'a>>> {
+    None.or_else(|| parser.try_arg_value().map(|arg_value| vec![*arg_value]))
+        .or_else(|| parser.try_mrhs())
+}
+fn try_exc_var<'a, C: Constructor>(
+    parser: &mut Parser<'a, C>,
+) -> Option<(Token<'a>, Box<Node<'a>>)> {
+    let assoc_t = parser.try_token(TokenValue::tASSOC)?;
+    if let Some(lhs) = parser.try_lhs() {
+        Some((assoc_t, lhs))
+    } else {
+        panic!("error: expected LHS after =>")
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::try_opt_rescue1;
     use crate::parser::RustParser;
 
     #[test]
     fn test_opt_rescue1() {
         let mut parser = RustParser::new(b"rescue");
-        assert_eq!(parser.try_opt_rescue1(), None);
+        assert_eq!(try_opt_rescue1(&mut parser), None);
     }
 }
