4e178ad2343b76e358c80aad5431878ba0b7c18a
Ilya Bylich <ibylich@gmail.com>
2022-07-12 19:08:49 +0200

    use Result<T, ParseError> instead of custom ParserResult

--- src/parser/result.rs
+++ src/parser/result.rs
@@ -1,114 +1,130 @@
 use crate::{lexer::Checkpoint, token::TokenKind};
 
-pub(crate) struct ParseResult<T> {
+#[derive(Debug)]
+pub(crate) struct ParseResultChain<T> {
     checkpoint: Checkpoint,
-    inner: ParseResultInner<T>,
+    inner: Result<T, ParseError>,
 }
 
-impl<T> ParseResult<T> {
+impl<T> ParseResultChain<T> {
     pub(crate) fn new(checkpoint: Checkpoint) -> Self {
         Self {
             checkpoint,
-            inner: ParseResultInner::Error {
-                expectations: vec![],
-            },
+            inner: Err(ParseError::empty()),
         }
     }
 
     pub(crate) fn or_else<F>(self, f: F) -> Self
     where
-        F: FnOnce() -> Result<T, Expectation>,
+        F: FnOnce() -> Result<T, ParseError>,
     {
         let Self { checkpoint, inner } = self;
-        match inner.or_else(f) {
-            ok @ ParseResultInner::Ok(_) => {
-                // match, no need to perform a rollback
+
+        match inner {
+            ok @ Ok(_) => {
+                // we had a match, no need to run anything
                 Self {
                     checkpoint,
                     inner: ok,
                 }
             }
-            err @ ParseResultInner::Error { .. } => {
-                // no match, rollback
-                checkpoint.restore();
-                Self {
-                    checkpoint,
-                    inner: err,
-                }
-            }
-        }
-    }
-
-    fn handle_error<F>(self, f: F) -> Option<T>
-    where
-        F: FnOnce(Vec<Expectation>) -> (),
-    {
-        self.inner.handle_error(f)
-    }
-}
-
-enum ParseResultInner<T> {
-    Ok(T),
-    Error { expectations: Vec<Expectation> },
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub(crate) enum Expectation {
-    ExpectedTokenToStartRule {
-        expected: TokenKind,
-        rule: &'static str,
-    },
-    ExpectedTokenAfterTokensToCompleteRule {
-        expected: TokenKind,
-        got: Vec<TokenKind>,
-    },
-}
-
-impl<T> ParseResultInner<T> {
-    fn or_else<F>(self, f: F) -> Self
-    where
-        F: FnOnce() -> Result<T, Expectation>,
-    {
-        match self {
-            ok @ Self::Ok(_) => ok,
-            Self::Error { mut expectations } => {
+            Err(mut error) => {
                 let fallback = f();
                 match fallback {
-                    Ok(value) => Self::Ok(value),
-                    Err(expectation) => {
-                        expectations.push(expectation);
-                        Self::Error { expectations }
+                    ok @ Ok(_) => {
+                        // we've got a match, return it
+                        Self {
+                            checkpoint,
+                            inner: ok,
+                        }
+                    }
+                    Err(mut new_error) => {
+                        // record errors
+                        error.expectations.append(&mut new_error.expectations);
+                        // perform a rollback
+                        checkpoint.restore();
+
+                        // and return an error
+                        Self {
+                            checkpoint,
+                            inner: Err(error),
+                        }
                     }
                 }
             }
         }
     }
 
-    fn handle_error<F>(self, f: F) -> Option<T>
+    pub(crate) fn into_parse_result(self) -> Result<T, ParseError> {
+        self.inner
+    }
+
+    pub(crate) fn handle_error<F>(self, f: F) -> Option<T>
     where
-        F: FnOnce(Vec<Expectation>) -> (),
+        F: FnOnce(ParseError) -> (),
     {
-        match self {
-            Self::Ok(value) => Some(value),
-            Self::Error { expectations } => {
-                f(expectations);
+        match self.inner {
+            Ok(value) => Some(value),
+            Err(error) => {
+                f(error);
                 None
             }
         }
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct ParseError {
+    pub(crate) expectations: Vec<Expectation>,
+}
+
+impl ParseError {
+    pub(crate) fn empty() -> Self {
+        Self {
+            expectations: vec![],
+        }
+    }
+
+    pub(crate) fn expected_token(expected: TokenKind, actual: TokenKind) -> Self {
+        Self {
+            expectations: vec![Expectation::ExpectedToken {
+                expected_token: expected,
+                got_token: actual,
+            }],
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum Expectation {
+    ExpectedToken {
+        expected_token: TokenKind,
+        got_token: TokenKind,
+    },
+    ExpectedRule {
+        rule: &'static str,
+        expected_token: TokenKind,
+        got_token: TokenKind,
+    },
+
+    // For initial lookahead
+    LooakaheadFailed {
+        expected_token: TokenKind,
+        got_tokeb: TokenKind,
+    },
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{Checkpoint, Expectation, ParseResult};
+    use super::{Checkpoint, Expectation, ParseError, ParseResultChain};
     use crate::{
         state::{OwnedState, StateRef},
         token::TokenKind,
     };
 
-    const EXPECTATION: Expectation = Expectation::ExpectedTokenToStartRule {
-        expected: TokenKind::tTEST_TOKEN,
-        rule: "test",
+    const EXPECTATION: Expectation = Expectation::ExpectedToken {
+        expected_token: TokenKind::tTEST_TOKEN,
+        got_token: TokenKind::kALIAS,
     };
 
     fn setup() -> (OwnedState, StateRef, Checkpoint, usize) {
@@ -121,7 +137,6 @@ mod tests {
 
         // create a checkpoint after altering state
         let checkpoint = Checkpoint::new(state_ref);
-        dbg!(&checkpoint);
 
         (state, state_ref, checkpoint, initial_pos)
     }
@@ -130,8 +145,8 @@ mod tests {
     fn test_result_ok() {
         let (state, state_ref, checkpoint, initial_pos) = setup();
 
-        let mut errors = vec![];
-        let result = ParseResult::new(checkpoint)
+        let mut errors = ParseError::empty();
+        let result = ParseResultChain::new(checkpoint)
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 1);
@@ -140,7 +155,12 @@ mod tests {
             .handle_error(|errs| errors = errs);
 
         assert_eq!(result, Some(1));
-        assert_eq!(errors, vec![]);
+        assert_eq!(
+            errors,
+            ParseError {
+                expectations: vec![]
+            }
+        );
         // make sure we DID NOT make a rollback
         assert_eq!(state.buffer().pos(), initial_pos + 1);
     }
@@ -149,17 +169,24 @@ mod tests {
     fn test_result_err() {
         let (state, state_ref, checkpoint, initial_pos) = setup();
 
-        let mut errors = vec![];
-        let result = ParseResult::<i32>::new(checkpoint)
+        let mut errors = ParseError::empty();
+        let result = ParseResultChain::<i32>::new(checkpoint)
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 1);
-                Err(EXPECTATION)
+                Err(ParseError {
+                    expectations: vec![EXPECTATION],
+                })
             })
             .handle_error(|errs| errors = errs);
 
         assert_eq!(result, None);
-        assert_eq!(errors, vec![EXPECTATION]);
+        assert_eq!(
+            errors,
+            ParseError {
+                expectations: vec![EXPECTATION]
+            }
+        );
         // make sure we performed a rollback
         assert_eq!(state.buffer().pos(), initial_pos);
     }
@@ -168,12 +195,14 @@ mod tests {
     fn test_result_mixed_ok_with_errors() {
         let (state, state_ref, checkpoint, initial_pos) = setup();
 
-        let mut errors = vec![];
-        let result = ParseResult::new(checkpoint)
+        let mut errors = ParseError::empty();
+        let result = ParseResultChain::new(checkpoint)
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 1);
-                Err(EXPECTATION)
+                Err(ParseError {
+                    expectations: vec![EXPECTATION],
+                })
             })
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
@@ -183,12 +212,19 @@ mod tests {
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 3);
-                Err(EXPECTATION)
+                Err(ParseError {
+                    expectations: vec![EXPECTATION],
+                })
             })
             .handle_error(|errs| errors = errs);
 
         assert_eq!(result, Some(42));
-        assert_eq!(errors, vec![]);
+        assert_eq!(
+            errors,
+            ParseError {
+                expectations: vec![]
+            }
+        );
         // make sure we DID NOT performe a rollback
         assert_eq!(state.buffer().pos(), initial_pos + 2);
     }
@@ -197,22 +233,31 @@ mod tests {
     fn test_result_multiple_errors() {
         let (state, state_ref, checkpoint, initial_pos) = setup();
 
-        let mut errors = vec![];
-        let result = ParseResult::<i32>::new(checkpoint)
+        let mut errors = ParseError::empty();
+        let result = ParseResultChain::<i32>::new(checkpoint)
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 1);
-                Err(EXPECTATION)
+                Err(ParseError {
+                    expectations: vec![EXPECTATION],
+                })
             })
             .or_else(|| {
                 assert_eq!(state_ref.buffer().pos(), initial_pos);
                 state_ref.buffer().set_pos(initial_pos + 2);
-                Err(EXPECTATION)
+                Err(ParseError {
+                    expectations: vec![EXPECTATION],
+                })
             })
             .handle_error(|errs| errors = errs);
 
         assert_eq!(result, None);
-        assert_eq!(errors, vec![EXPECTATION, EXPECTATION]);
+        assert_eq!(
+            errors,
+            ParseError {
+                expectations: vec![EXPECTATION, EXPECTATION]
+            }
+        );
         // make sure we DID NOT performe a rollback
         assert_eq!(state.buffer().pos(), initial_pos);
     }
