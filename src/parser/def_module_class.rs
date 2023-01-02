92c84e21f21712e6cff84c3b7bc531be2ae684d5
Ilya Bylich <ibylich@gmail.com>
2023-01-02 19:22:39 +0100

    parse inheritance

--- src/parser/def_module_class.rs
+++ src/parser/def_module_class.rs
@@ -1,5 +1,6 @@
 use crate::{
     builder::Builder,
+    nodes::Send,
     parser::{base::Rule, Bodystmt, TermT, Value},
     token::{Token, TokenKind},
     Node, Parser,
@@ -15,7 +16,19 @@ impl Rule for Module {
 
     fn parse(parser: &mut Parser) -> Self::Output {
         let module_t = parser.take_token();
-        let name = CPath::parse(parser);
+        let name = match CPath::parse(parser) {
+            CPath::Const(name) => name,
+            CPath::Inheritance {
+                name,
+                lt_t,
+                superclass,
+            } => {
+                panic!(
+                    "Inheritance in module declaration: {:?} {:?} {:?}",
+                    name, lt_t, superclass
+                )
+            }
+        };
         let body = Bodystmt::parse(parser);
         let end_t = parser.expect_token(TokenKind::kEND);
         Builder::def_module(module_t, name, body, end_t)
@@ -41,8 +54,14 @@ impl Rule for Class {
             let end_t = parser.expect_token(TokenKind::kEND);
             Builder::def_sclass(class_t, lshift_t, expr, body, end_t)
         } else {
-            let name = CPath::parse(parser);
-            let (lt_t, superclass) = Superclass::parse(parser);
+            let (name, lt_t, superclass) = match dbg!(CPath::parse(parser)) {
+                CPath::Const(name) => (name, None, None),
+                CPath::Inheritance {
+                    name,
+                    lt_t,
+                    superclass,
+                } => (name, Some(lt_t), Some(superclass)),
+            };
             let body = Bodystmt::parse(parser);
             let end_t = parser.expect_token(TokenKind::kEND);
             Builder::def_class(class_t, name, lt_t, superclass, body, end_t)
@@ -50,28 +69,18 @@ impl Rule for Class {
     }
 }
 
-struct Superclass;
-impl Rule for Superclass {
-    type Output = (Option<Token>, Option<Box<Node>>);
+#[derive(Debug)]
+enum CPath {
+    Const(Box<Node>),
 
-    fn starts_now(_parser: &mut Parser) -> bool {
-        true
-    }
-
-    fn parse(parser: &mut Parser) -> Self::Output {
-        if !parser.current_token().is(TokenKind::tLT) {
-            return (None, None);
-        }
-        let lt_t = parser.take_token();
-        let superclass = Value::parse(parser);
-        let _ = TermT::parse(parser);
-        (Some(lt_t), Some(superclass))
-    }
+    Inheritance {
+        name: Box<Node>,
+        lt_t: Token,
+        superclass: Box<Node>,
+    },
 }
-
-struct CPath;
 impl Rule for CPath {
-    type Output = Box<Node>;
+    type Output = Self;
 
     fn starts_now(parser: &mut Parser) -> bool {
         Value::starts_now(parser)
@@ -79,8 +88,35 @@ impl Rule for CPath {
 
     fn parse(parser: &mut Parser) -> Self::Output {
         let node = Value::parse(parser);
-        // TODO: check that node is Const
-        node
+
+        if matches!(&*node, Node::Const(_)) {
+            return Self::Const(node);
+        }
+
+        match *node {
+            Node::Send(Send {
+                recv: Some(recv),
+                method_name,
+                args,
+                selector_l: Some(selector_l),
+                ..
+            }) if method_name.as_bytes() == b"<" && args.len() == 1 => {
+                let name = recv;
+                let lt_t = Token {
+                    kind: TokenKind::tLT,
+                    loc: selector_l,
+                    value: None,
+                };
+                let superclass = Box::new(args.into_iter().next().unwrap());
+                Self::Inheritance {
+                    name,
+                    lt_t,
+                    superclass,
+                }
+            }
+
+            other => panic!("Wrong type of CPath: {:?}", other),
+        }
     }
 }
 
@@ -109,8 +145,8 @@ fn test_class() {
         b"class Foo < Bar; 1; end",
         r#"
 s(:class,
-  s(:const,
-    s(:const, nil, "Foo"), "Bar"), nil,
+  s(:const, nil, "Foo"),
+  s(:const, nil, "Bar"),
   s(:int, "1"))
             "#
     )
--- src/parser/value/builders.rs
+++ src/parser/value/builders.rs
@@ -1,4 +1,4 @@
-use crate::{Node, Parser, Token};
+use crate::{builder::Builder, parser::Value, Node, Parser, Token, TokenKind};
 
 pub(crate) fn build_prefix_op(op_t: Token, arg: Box<Node>, parser: &mut Parser) -> Box<Node> {
     todo!()
@@ -10,7 +10,12 @@ pub(crate) fn build_binary_op(
     parser: &mut Parser,
     r_bp: u8,
 ) -> Box<Node> {
-    todo!()
+    let rhs = Value::parse_bp(parser, r_bp);
+
+    match op_t.kind {
+        TokenKind::tLT | TokenKind::tGT => Builder::binary_op(lhs, op_t, rhs, parser.buffer()),
+        _ => todo!("{:?} {:?} {:?}", lhs, op_t, rhs),
+    }
 }
 
 pub(crate) fn build_postfix_op(op_t: Token, arg: Box<Node>, parser: &mut Parser) -> Box<Node> {
